726d8fc608c8160513b4822445b92abb5b0b797e
dhvanish.thakkar <dhvanishthakkar@gmail.com>
2024-05-22 20:46:02 +0530

    Added few more test scenario and optimize the approach, Also calculated time and space complexity

--- DSA/BackTracking/N_Queens.cc
+++ DSA/BackTracking/N_Queens.cc
@@ -20,48 +20,43 @@ using namespace std;
  *   - Each solution contains a distinct board configuration of the n-queens' placement,
  *     where 'Q' and '.' both indicate a queen and an empty space, respectively.
  *
- *
  *  Example : 
  *   - Input: n = 4  ==> Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ *   - Input: n = 1  ==> Output: [["Q"]]
  *
  *  LeetCode : 51
  *
  *  Solution :
  *   - We can solve this problem using backtracking
  *
- *  Time  Complexity :
- *  Space Complexity : 
+ *  Time  Complexity : O(N^2)
+ *  Space Complexity : O(N)
  *
  *******************************************************************************/
-vector<vector<string>>ans;
 
-bool isSafe(int i, int j, vector<string>&board)
-{
+bool isSafe(int i, int j, vector<string> &board) {
 	int x = i, y = j;
 	// top left diagonal
-	while(x >= 0 && y >= 0)
-	{
+	while (x >= 0 && y >= 0) {
 		if (board[x][y] == 'Q')
-		   return false;
+			return false;
 		x--;
-	    y--;
+		y--;
 	}
 
 	// Upwards
 	x = i;
 	y = j;
-	while(x >= 0)
-	{
+	while (x >= 0) {
 		if (board[x][y] == 'Q')
 			return false;
 		x--;
-	}	
+	}
 
 	// top right
 	x = i;
 	y = j;
-	while( x >= 0 && y < board.size())
-	{
+	while (x >= 0 && y < board.size()) {
 		if (board[x][y] == 'Q')
 			return false;
 		y++;
@@ -70,45 +65,66 @@ bool isSafe(int i, int j, vector<string>&board)
 	return true;
 }
 
-void N_Queens(int row, int n, vector<string> &board)
-{
-	if (row == n)
-	{
+void N_Queens(int row, int n, vector<string> &board,
+		vector<vector<string>> &ans) {
+	if (row == n) {
 		ans.push_back(board);
 		return;
 	}
-	
-	for (int j = 0; j < n ; j++)
-	{
-		if (isSafe(row, j, board))
-		{
+
+	for (int j = 0; j < n; j++) {
+		if (isSafe(row, j, board)) {
 			//place the queen
 			board[row][j] = 'Q';
-			N_Queens(row+1, n, board);
-			board[row][j]='.';
+			N_Queens(row + 1, n, board, ans);
+			board[row][j] = '.';
 		}
 	}
+
+	return;
 }
 
 // Main Function
-int main()
-{
+int main() {
 	// Testcase 1
+	vector<vector<string>> ans1;
 	int row = 0, n = 4;
+
+	// Initialize output array with '.'
 	string temp;
 	for (int i = 0; i < n; i++)
 		temp += '.';
-	vector<string> board(n, temp);
-	
-	// Function call
-	N_Queens(row, n, board);
+	vector<string> board1(n, temp);
 
-	for(auto output : ans)
-	{
-		for (auto data :output)
-		    cout << data << endl;
-	    cout << endl;
+	// Function call
+	N_Queens(row, n, board1, ans1);
+
+	for (auto output : ans1) {
+		for (auto data : output)
+			cout << data << endl;
+		cout << endl;
 	}
 	cout << endl;
+
+	cout << "=================================" << endl;
+
+	// Testcase 2
+	vector<vector<string>> ans2;
+	n = 1;
+	// Initialize output array with '.'
+	for (int i = 0; i < n; i++)
+		temp += '.';
+	vector<string> board2(n, temp);
+
+	// Function call
+	N_Queens(0, n, board2, ans2);
+
+	for (auto output : ans2) {
+		for (auto data : output)
+			cout << data << endl;
+		cout << endl;
+	}
+	cout << endl;
+
 	return 0;
 }
